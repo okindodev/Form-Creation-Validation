f311e407426233ca79162e13bcdd65690696180e
Okindo <okindo.dev@gmail.com>
2025-08-08 08:25:11 +0300

    advanced JS properties and values

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const form = document.getElementById('registraion-form');
+document.addEventListener('DOMContentLoaded',function(){
+    const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById("form-feedback")
     form.addEventListener("submit", (event)=>{
         event.preventDefault();
 
     const usernameInput = document.getElementById("username").value.trim();
-    const emailInpput = document.getElementById("email").value.trim();
+    const emailInput = document.getElementById("email").value.trim();
     const passwordInput = document.getElementById("password").value.trim();
     
 
@@ -16,12 +16,12 @@ document.addEventListener('DOMContentLoaded',()=>{
         messages.push("Your username is too short");
     }
 
-    if (!emailInpput.includes('@') || !emailInpput.includes('.')){
+    if (!emailInput.includes('@') || !emailInput.includes('.')){
         isValid= false;
         messages.push("Include @ or .");
 
     }
-    if (passwordInput < 8){
+    if (passwordInput.length < 8){
         isValid = false;
     messages.push("Your password should be atleast 8 characters long");
     }
